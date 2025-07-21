f52e2061ea49ef3615cd31f11154673fcbe657bf
yyrrcc <music_v@naver.com>
2025-07-21 11:47:17 +0900

    주석 추가 및 코드 개선

--- src/test/Test04.java
+++ src/test/Test04.java
@@ -3,14 +3,14 @@ package test;
 public class Test04 {
 // 1부터 100까지의 수 중 홀수 개수와 짝수 개수를 출력
 	public static void main(String[] args) {
-		int even = 0;
-		int odd = 0;
+		int even = 0; // evenCount 변수이름
+		int odd = 0; // oddCount
 
 		for (int i = 1; i <= 100; i++) {
 			if (i % 2 == 0) {
-				even += 1;
+				even += 1; // evenCount++;
 			} else {
-				odd += 1;
+				odd += 1; // evenCount++;
 			}
 		}
 
--- src/test/Test05.java
+++ src/test/Test05.java
@@ -3,10 +3,18 @@ package test;
 public class Test05 {
 // 1부터 50까지 수 중 50의 약수를 모두 출력
 	public static void main(String[] args) {
-		int n = 50;
-		for (int i = 1; i <= 50; i++) {
-			if (n % i == 0) {
-				System.out.println(i);
+//		int n = 50;
+//		for (int i = 1; i <= 50; i++) {
+//			if (n % i == 0) {
+//				System.out.println(i);
+//			}
+//		}
+
+		// 개선한 코드
+		int x = 50;
+		for (int j = 1; j <= x; j++) {
+			if (x % j == 0) {
+				System.out.println(j);
 			}
 		}
 
